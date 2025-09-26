989a12a52ccc315cc0ce56f26d57e4f760751709
liatlas <atlasli2016@gmail.com>
2025-09-25 23:54:18 -0400

    possible complete version of task2

--- ATLAS_LI_HW1_Task2.py
+++ ATLAS_LI_HW1_Task2.py
@@ -9,43 +9,101 @@ You are asked to simulate this feature as a Python console application. The appl
 
 3. VISIT <url>: Push the current page on the top of the backward stack, and make the URL specify the new current page. The forward stack is emptied.
 
-4. QUIT: Quit the application. Consider handling any exceptional scenarios (e.g. invalid attempts to go BACK or FORWARD, missing ‘url’ for the visit command, misspelled/invalid commands etc.).
+4. QUIT: Quit the application. 
+
+Consider handling any exceptional scenarios (e.g. invalid attempts to go BACK or FORWARD, missing ‘url’ for the visit command, misspelled/invalid commands etc.).
 
 Input
 Input contains a series of commands. The keywords BACK, FORWARD, VISIT, and QUIT are all in uppercase. URLs have no whitespace and have at most 50 characters. The end of input is indicated by the QUIT command and the application should terminate when this command is received.
 
 Output For each command, print the URL of the current page (in a line) after the command is executed if the command is not ignored. Otherwise, print Ignored
 """
+import sys
+from typing import Optional, List
 
 class TwoStack:
 
     def __init__(self):
 
-        self._fstack = []
-        self._bstack = []
+        self._fstack: List[str] = []
+        self._bstack: List[str] = []
         self._current = None
     
-    def back(self):
-        if self._bstack == []:
-            raise 
-        self._fstack.pop
+    def back(self) -> Optional[str]:
+
+        if self._bstack != []:
+
+            self._fstack.append(self._current)
+            self._current  = self._bstack.pop()
+
+            print(self._current)
+
+        else:
+
+            print("Ignored")
+
+    def forward(self) -> Optional[str]:
+
+        if self._fstack != []:
+
+            self._bstack.append(self._current)
+            self._current = self._fstack.pop()
+
+            print(self._current)
+        
+        else:
+
+            print("Ignored")
 
 
-    def forward(self):
-        print("forward")
-    def visit(self, link):
-        print("visit")
+    def visit(self, link: str) -> str:
+
+        self._bstack.append(self._current)
+        self._current = link
+
+        while self._fstack:
+
+            self._fstack.pop()
+
+        print(self._current)
+
     def quit(self):
-        pass
+        sys.exit(0)
+    
 
 if __name__ == "__main__":
-    
+
     ts1 = TwoStack()
 
-    print(--Enter the site that you want to visit--)
-    u_input = input()
+    command_list = dict({"FOWARD": ts1.forward, "BACKWARD": ts1.back, "VISIT": ts1.visit, "QUIT": ts1.quit})
 
-    while u_input != "QUIT":
-    
+    print("--Enter the site that you want to visit--")
+
+    while True:
         
+        command = None
+        arg = None
 
+        i = input().strip().split(maxsplit = 1)
+
+        command = str(i[0]) if i else None
+        arg = i[1] if len(i) > 1 else None
+
+        if arg and not (len(arg) < 50 and " " not in arg):
+
+            print("--Please print a valid command - url cannot contain spaces or be greater than 50 characters--")
+            continue
+
+        if command in command_list.keys():
+
+            func = command_list[command]
+
+        else:
+
+            print("--Please print a valid command--")
+            continue
+            
+        if arg is not None:
+            func(arg)
+        else:
+            func()
